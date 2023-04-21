35858d19dc77801cc756a47f28c54b723b2d4676
nikita <yeliseev2003@mail.ru>
2023-04-21 13:26:06 +0700

    download and upload files

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.types import ParseMode, ContentTypes
+from aiogram.types import ParseMode, ContentTypes, InputFile
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -63,24 +63,34 @@ async def get_zapros(message: types.Message):
 
 @dp.message_handler(content_types=ContentTypes.DOCUMENT)
 async def unknown_message(message: types.Message):
+    logging.info(f'uploading file to server')
     file_name = message.document.file_name
     #my_file = open(file_name, "w+")
     print(message.document)
     result = await bot.download_file_by_id(message.document.file_id)
-    print(bytes(result.read()))
+    #print(result)
+    #print(bytes(result.read()))
+    #fail = InputFile(filename="aboba", path_or_bytesio=result)
+    #print(fail)
+    result = bytes(result.read())
+    #print(result)
     #await bot.download_file('.')
-
     #typing.BinaryIO
     #my_file.write(message.document)
     #my_file.close()
     await bot.send_message(message.from_user.id, file_name)
+    #await bot.send_file(message.document.mime_type,file=result)
+    #await bot.send_document(message.from_user.id, document=InputFile(filename=message.document.file_name, path_or_bytesio=await bot.download_file_by_id(message.document.file_id)))
+    await bot.send_document(message.from_user.id, document=result)
+
 
 
 @dp.message_handler(commands=['bot_ip'])
 async def get_ip(message: types.Message):
     my_ip = requests.get("http://api.myip.com")
     logging.info(f'wants ip {time.asctime()} {message.from_user.username}')
-    await bot.send_message(message.from_user.id, json.loads(my_ip.text))
+    print(my_ip.text)
+    await bot.send_message(message.from_user.id, my_ip.text)
 
 if __name__ == '__main__':
     print('bebra')
